6891b30ef4ee7038603098f0804d7478b3d80158
mahdiisabry1 <mahdiisabry@gmail.com>
2025-03-22 21:25:54 +0530

    Section

---
+++ client/src/components/Sections.jsx
@@ -0,0 +1,14 @@
+const Sections = () => {
+  return (
+    <>
+      <div className="grid grid-cols-4 grid-rows-4 gap-4 items-center">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </div>
+    </>
+  );
+};
+
+export default Sections;
--- client/src/models/Home.jsx
+++ client/src/models/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../components/Banner";
+import Sections from "../components/Sections";
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <Sections />
     </>
   );
 };
